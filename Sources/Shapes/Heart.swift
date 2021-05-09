a3f3833db9d28281a15c129da44516600746233c
Nutter Fi <itsnutterfi@gmail.com>
2021-05-08 22:41:13 -0600

    Add Heart shape

---
+++ Sources/Shapes/Heart.swift
@@ -0,0 +1,48 @@
+//
+//  Heart.swift
+//  
+//
+//  Created by nutterfi on 5/8/21.
+//
+
+import SwiftUI
+
+public struct Heart: Shape {
+  // normalized
+  var origin: CGPoint { CGPoint(x: 0.5, y: 0.25) }
+  var bottom: CGPoint { CGPoint(x: 0.5, y: 1) }
+  var controlLeft1: CGPoint { CGPoint(x: 0.2, y: -0.15) }
+  var controlLeft2: CGPoint { CGPoint(x: -0.25, y: 0.45) }
+  var controlRight1: CGPoint { CGPoint(x: 0.8, y: -0.15) }
+  var controlRight2: CGPoint { CGPoint(x: 1.25, y: 0.45) }
+
+  public func path(in rect: CGRect) -> Path {
+    
+    func scaledPoint(_ point: CGPoint) -> CGPoint {
+      CGPoint(x: point.x * rect.width, y: point.y * rect.height)
+    }
+    
+    return Path { path in
+      path.move(to: scaledPoint(origin))
+      path.addCurve(to: scaledPoint(bottom), control1: scaledPoint(controlLeft1), control2: scaledPoint(controlLeft2))
+      path.addCurve(to: scaledPoint(origin), control1: scaledPoint(controlRight2), control2: scaledPoint(controlRight1))
+      path.closeSubpath()
+    }
+  }
+    
+}
+
+struct Heart_Previews: PreviewProvider {
+    static var previews: some View {
+      VStack {
+        Heart()
+          .stroke(lineWidth: 20)
+        Divider()
+        Image(systemName: "heart")
+          .resizable()
+          .scaledToFit()
+      }
+      .padding()
+      
+    }
+}
--- Tests/ShapesTests/DiamondTests.swift
+++ Tests/ShapesTests/DiamondTests.swift
@@ -2,14 +2,21 @@ import XCTest
 @testable import Shapes
 
 final class DiamondTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-      XCTAssertEqual(Diamond(pointRatio: 0.25).pointRatio, 0.25)
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  
+  /// WISH LIST
+  // TODO: How can I use a rect in Shape for scaling normalized CGPoints?
+  protocol NFiShape: Shape {
+    func scaledPoint(_ point: CGPoint) -> CGPoint
+  }
+  
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(Diamond(pointRatio: 0.25).pointRatio, 0.25)
+  }
+  
+  static var allTests = [
+    ("testExample", testExample),
+  ]
 }
