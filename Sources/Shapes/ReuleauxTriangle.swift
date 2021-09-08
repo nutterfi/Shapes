ab9a187efe1a8835e05aaa4a2a09ad954a151145
Nutter Fi <itsnutterfi@gmail.com>
2021-09-07 23:04:22 -0600

    Drawing Reuleaux Triangle clockwise

--- Sources/Shapes/ReuleauxTriangle.swift
+++ Sources/Shapes/ReuleauxTriangle.swift
@@ -16,12 +16,12 @@ struct ReuleauxTriangle: Shape {
       let dx = abs(points[1].x - points[0].x)
       let dy = abs(points[1].y - points[0].y)
       let r = sqrt(dx * dx + dy * dy)
-      
-      path.addRelativeArc(center: points[2], radius: r, startAngle: .degrees(-270), delta: .degrees(-60))
-      
-      path.addRelativeArc(center: points[1], radius: r, startAngle: .degrees(-30), delta: .degrees(-60))
 
-      path.addRelativeArc(center: points[0], radius: r, startAngle: .degrees(-150), delta: .degrees(-60))
+      path.addRelativeArc(center: points[0], radius: r, startAngle: .degrees(150), delta: .degrees(60))
+
+      path.addRelativeArc(center: points[1], radius: r, startAngle: .degrees(270), delta: .degrees(60))
+
+      path.addRelativeArc(center: points[2], radius: r, startAngle: .degrees(30), delta: .degrees(60))
       
     }
     .rotation(.degrees(-90)).path(in: rect)
@@ -33,6 +33,7 @@ struct ReuleauxTriangle_Previews: PreviewProvider {
   static var previews: some View {
     ZStack {
       ReuleauxTriangle()
+        .stroke()
         .foregroundColor(.yellow)
     }
     .frame(width: 256, height: 256)
