4829c10c214cac5d2613b85726932e627a968167
Nutter Fi <itsnutterfi@gmail.com>
2021-09-13 23:08:31 -0400

    Resolved the general case for Reuleaux Polygons
    - assume regular polygons, where N is odd. Even numbered polygons will produce an interesting result as well.

--- Sources/Shapes/ReuleauxPolygon.swift
+++ Sources/Shapes/ReuleauxPolygon.swift
@@ -5,64 +5,53 @@
 //
 
 import SwiftUI
-// WIP
-// WARNING: DO NOT USE
-struct ReuleauxPolygon: Shape {
-  var sides: Int // MUST BE ODD
 
-  /** N-sided Ex. Pentagon
-      0 -> 2, 3    (i + 2, i+3) % N
-      1 -> 3,4
-      2 -> 4, 0
-      3 -> 0, 1
-      4 -> 1, 2
-        
-   N-sided polygon
-   7-sided
-      0 -> 3, 4   0 -> [(N-1)/2, (N-1)/2 + 1] % N
-   jth index -> ((N-1)/2 + j, (N-1)/2 + 1 + j) % N
-   9-sided
-   0 -> 4,5
-   */
+/**
+ A curve of constant width made up of circular arcs of constant radius. This implementation assumes odd-number sided regular polygons
+ */
+public struct ReuleauxPolygon: Polygon {
   
-  func path(in rect: CGRect) -> Path {
+  public func vertices(in rect: CGRect) -> [CGPoint] {
+    ConvexPolygon(sides: sides).vertices(in: rect)
+  }
+  
+  /// must be odd
+  public var sides: Int
+  
+  public func path(in rect: CGRect) -> Path {
     Path { path in
-      let points = ConvexPolygon(sides: sides).vertices(in: rect)
-      
-      path.move(to: points[0])
-      
-      // polar coordinates
+      let points = vertices(in: rect)
+            
+      // length from furthest points
       let dx = abs(points[(sides-1)/2].x - points[0].x)
       let dy = abs(points[(sides-1)/2].y - points[0].y)
       let r = sqrt(dx * dx + dy * dy)
       
-      // e.g. for a triangle, adding 120 degrees each time, is the opposite angle of the internal angle of the triangle (60deg)
-      let internalAngleDegrees: CGFloat = 180.0 / CGFloat(sides)
-      // 36 deg for a pentagon
+      // arc from point to point on the unit circle
+      let arc: CGFloat = 2 * .pi / CGFloat(sides)
       
-      // start internalAngle + half the internal angle
+      // arc length centered at first position
+      let delta = .pi / CGFloat(sides)
       
-      // while there are still vertices remaining, hop to the next vertex and repeat the process of adding an arc from the start angle to the end angle which is
+      // start angle at first position
+      let a0 = .pi - (.pi / (2.0 * CGFloat(sides)))
       
-      path.addRelativeArc(center: points.first!, radius: r, startAngle: .degrees(-180 + internalAngleDegrees*0.5), delta: .degrees(-internalAngleDegrees), transform: .init(rotationAngle: 0))
-      
-      path.addRelativeArc(center: points[0], radius: r, startAngle: .degrees(-180 + internalAngleDegrees*0.5), delta: .degrees(-internalAngleDegrees), transform:
-                              .init(rotationAngle: Angle.degrees(internalAngleDegrees).radians))
-      
-//      path.addRelativeArc(center: points[2], radius: r, startAngle: .degrees(-270), delta: .degrees(-60))
-      
-      path.addRelativeArc(center: points[1], radius: r, startAngle: .degrees(-30), delta: .degrees(-60))
+      // the arc is drawn for each side of the polygon where the center is the opposite point
+      for i in 0..<sides {
+        let startAngle = a0 + CGFloat(i) * arc
+        path.addRelativeArc(center: points[i], radius: r, startAngle: .radians(startAngle), delta: .radians(delta))
+      }
+      path.closeSubpath()
       
     }
     .rotation(.degrees(-90)).path(in: rect)
   }
-  
-  
     
 }
 
 struct ReuleauxPolygon_Previews: PreviewProvider {
     static var previews: some View {
-      ReuleauxPolygon(sides: 3)
+      ReuleauxPolygon(sides: 5)
+        .stroke()
     }
 }
--- Sources/Shapes/ReuleauxTriangle.swift
+++ Sources/Shapes/ReuleauxTriangle.swift
@@ -9,22 +9,7 @@ import SwiftUI
 
 struct ReuleauxTriangle: Shape {
   func path(in rect: CGRect) -> Path {
-    Path { path in
-      let points = ConvexPolygon(sides: 3).vertices(in: rect)
-            
-      // polar coordinates
-      let dx = abs(points[1].x - points[0].x)
-      let dy = abs(points[1].y - points[0].y)
-      let r = sqrt(dx * dx + dy * dy)
-
-      path.addRelativeArc(center: points[0], radius: r, startAngle: .degrees(150), delta: .degrees(60))
-
-      path.addRelativeArc(center: points[1], radius: r, startAngle: .degrees(270), delta: .degrees(60))
-
-      path.addRelativeArc(center: points[2], radius: r, startAngle: .degrees(30), delta: .degrees(60))
-      
-    }
-    .rotation(.degrees(-90)).path(in: rect)
+    ReuleauxPolygon(sides: 3).path(in: rect)
   }
   
 }
