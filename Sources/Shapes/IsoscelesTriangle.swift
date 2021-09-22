9b5a6fe35046595eb64b4f6593a66b68a9ad2694
Nutter Fi <itsnutterfi@gmail.com>
2021-09-22 12:25:25 -0700

    Added Isosceles and Right Triangle Shapes. Conform to InsettableShape

---
+++ Sources/Shapes/IsoscelesTriangle.swift
@@ -0,0 +1,50 @@
+//
+//  IsoscelesTriangle.swift
+//  
+//
+//  Created by nutterfi on 9/22/21.
+//
+
+import SwiftUI
+
+public struct IsoscelesTriangle: Shape {
+  
+  var inset: CGFloat = .zero
+
+  public func path(in rect: CGRect) -> Path {
+    let aRect = rect.insetBy(dx: inset, dy: inset)
+    return Path { path in
+      path.move(to: CGPoint(x: aRect.midX, y: aRect.minY))
+      path.addLine(to: CGPoint(x: aRect.maxX, y: aRect.maxY))
+      path.addLine(to: CGPoint(x: aRect.minX, y: aRect.maxY))
+      path.closeSubpath()
+    }
+  }
+}
+
+extension IsoscelesTriangle: InsettableShape {
+  
+  public func inset(by amount: CGFloat) -> some InsettableShape {
+    var triangle = self
+    triangle.inset += amount
+    return triangle
+  }
+}
+
+struct IsoscelesTriangle_Previews: PreviewProvider {
+    static var previews: some View {
+      VStack(spacing: 0) {
+        let inset: CGFloat = 90
+        IsoscelesTriangle()
+          .border(Color.purple)
+        ZStack {
+          Rectangle().inset(by: inset).stroke()
+        IsoscelesTriangle()
+          .inset(by: inset)
+          .border(.purple, width: 10)
+        }
+      }
+      
+      
+    }
+}
---
+++ Sources/Shapes/RightTriangle.swift
@@ -0,0 +1,48 @@
+//
+//  RightTriangle.swift
+//  
+//
+//  Created by nutterfi on 9/22/21.
+//
+
+import SwiftUI
+
+public struct RightTriangle: Shape {
+  
+  var inset: CGFloat = .zero
+  
+  public func path(in rect: CGRect) -> Path {
+    let aRect = rect.insetBy(dx: inset, dy: inset)
+    return Path { path in
+      path.move(to: CGPoint(x: aRect.maxX, y: aRect.minY))
+      path.addLine(to: CGPoint(x: aRect.maxX, y: aRect.maxY))
+      path.addLine(to: CGPoint(x: aRect.minX, y: aRect.maxY))
+      path.closeSubpath()
+    }
+  }
+}
+
+extension RightTriangle: InsettableShape {
+  
+  public func inset(by amount: CGFloat) -> some InsettableShape {
+    var triangle = self
+    triangle.inset += amount
+    return triangle
+  }
+}
+
+struct RightTriangle_Previews: PreviewProvider {
+  static var previews: some View {
+    VStack(spacing: 0) {
+      let inset: CGFloat = 90
+      RightTriangle()
+        .border(Color.purple)
+      ZStack {
+        Rectangle().inset(by: inset).stroke()
+        RightTriangle()
+          .inset(by: inset)
+          .border(.purple, width: 10)
+      }
+    }
+  }
+}
