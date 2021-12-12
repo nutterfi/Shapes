4f70a684687130df92ec24129bd63e8010c2d8c2
Nutter Fi <itsnutterfi@gmail.com>
2021-12-11 21:34:41 -0800

    Teardrop now uses a CGPoint variable for the control point - breaking change!

--- Sources/Shapes/Teardrop.swift
+++ Sources/Shapes/Teardrop.swift
@@ -9,48 +9,106 @@ import SwiftUI
 public struct Teardrop: NFiShape {
   
   public var inset: CGFloat = .zero
+    
+  /// clamped values between 0...1
+  public var variation: CGPoint
   
-  // TODO: Consider using a CGPoint instead for better customization
-  // FIXME: Better usage of space
-  public var variation: CGFloat
-  
-  public init(_ variation: CGFloat = 0.5) {
-    self.variation = max(0, min(variation, 1))
+  public init(_ variation: CGPoint = CGPoint(x: CGFloat(0.5), y: CGFloat(0.5))) {
+    let x = variation.x.clamped(to: CGFloat(0.0)...CGFloat(1.0))
+    let y = variation.y.clamped(to: CGFloat(0.0)...CGFloat(1.0))
+    self.variation = CGPoint(x: x, y: y)
   }
   
   public func path(in rect: CGRect) -> Path {
-    let aRect = rect.insetBy(dx: inset, dy: inset)
+    let insetRect = rect.insetBy(dx: inset, dy: inset)
     
-    let dim = min(aRect.width, aRect.height)
-    let midPoint = CGPoint(x: aRect.midX, y: aRect.midY)
-    let origin = CGPoint(x: aRect.midX, y: aRect.minY)
+    let dim = min(insetRect.width, insetRect.height)
+    let midPoint = CGPoint(x: insetRect.midX, y: insetRect.midY)
+    let origin = CGPoint(x: insetRect.midX, y: insetRect.minY)
+    
+    let controlPoint1 = CGPoint(
+      x: insetRect.midX + insetRect.width * 0.5 * variation.x,
+      y: insetRect.minY + insetRect.height * 0.5 * variation.y
+    )
+    
+    let controlPoint2 = CGPoint(
+      x: insetRect.midX - insetRect.width * 0.5 * variation.x,
+      y: insetRect.minY + insetRect.height * 0.5 * variation.y
+    )
     
     return Path { path in
       path.move(to: origin)
       
-      path.addQuadCurve(to: CGPoint(x: aRect.midX + dim * 0.5, y: aRect.midY), control: CGPoint(x: aRect.midX + dim * 0.5, y: aRect.midY * variation))
+      path.addQuadCurve(
+        to: CGPoint(
+          x: insetRect.midX + dim * 0.5,
+          y: insetRect.midY
+        ),
+        control: controlPoint1
+      )
       
-      path.addArc(center: midPoint, radius: dim * 0.5, startAngle: .zero, endAngle: Angle(radians: .pi), clockwise: false)
+      path.addArc(
+        center: midPoint,
+        radius: dim * 0.5,
+        startAngle: .zero,
+        endAngle: Angle(radians: .pi),
+        clockwise: false)
       
-      path.addQuadCurve(to: origin, control: CGPoint(x: aRect.midX - dim * 0.5, y: aRect.midY * variation))
+      path.addQuadCurve(
+        to: origin,
+        control: controlPoint2
+      )
+      
+      let bounding = path.boundingRect
+      
+      path = path
+        .offsetBy(dx: insetRect.midX - bounding.midX, dy: insetRect.midY - bounding.midY)
+      
       path.closeSubpath()
+      
     }
   }
   
 }
 
+struct TeardropDemo: View {
+  @State private var xValue: CGFloat = 0.0
+  @State private var yValue: CGFloat = 0.0
+  
+  var body: some View {
+    VStack {
+      Slider(value: $xValue)
+      Slider(value: $yValue)
+      Spacer()
+      ZStack {
+        Teardrop(CGPoint(x: CGFloat(xValue), y: CGFloat(yValue)))
+          .strokeBorder(Color.red,
+                        style: StrokeStyle(
+                          lineWidth: 2,
+                          lineCap: .round,
+                          lineJoin: .round)
+          )
+        
+          .frame(width: 300, height: 300)
+          .border(Color.purple)
+        Teardrop(CGPoint(x: CGFloat(xValue), y: CGFloat(yValue)))
+          .inset(by: 80)
+          .strokeBorder(Color.red,
+                        style: StrokeStyle(
+                          lineWidth: 2,
+                          lineCap: .round,
+                          lineJoin: .round)
+          )
+          .frame(width: 300, height: 300)
+          .border(Color.purple)
+      }
+      Spacer()
+    }
+  }
+}
+
 struct Teardrop_Previews: PreviewProvider {
     static var previews: some View {
-      ZStack {
-        Teardrop()
-            .rotation(Angle(radians: .pi))
-            .stroke(Color.purple)
-        Teardrop()
-          .inset(by: 32)
-            .rotation(Angle(radians: .pi))
-            .stroke(Color.purple)
-      }
-      .border(Color.black)
-      .frame(width: 256, height: 512)
+      TeardropDemo()
     }
 }
