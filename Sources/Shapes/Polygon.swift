d552e782c7fae6ac0f153d6cfc8f7e738e3e75fd
Nutter Fi <itsnutterfi@gmail.com>
2022-06-07 20:19:52 -0700

    Changes to support compiler in iOS16.0 beta
    It got confused with Doubles and CGFloats

--- Sources/Shapes/Polygon.swift
+++ Sources/Shapes/Polygon.swift
@@ -38,9 +38,10 @@ public extension RegularPolygon {
   func vertices(in rect: CGRect, offset: Angle = .zero) -> [CGPoint] {
     let r = min(rect.size.width, rect.size.height) / 2
     let origin = CGPoint(x: rect.midX, y: rect.midY)
-    return Array(0 ..< sides).map {
-      let theta = 2 * .pi * CGFloat($0) / CGFloat(sides) + offset.radians - .pi / 2  // pointing north!
+    let array: [CGPoint] = Array(0 ..< sides).map {
+      let theta = 2 * .pi * CGFloat($0) / CGFloat(sides) + CGFloat(offset.radians) - CGFloat.pi / 2  // pointing north!
       return CGPoint(x: origin.x + r * cos(theta), y: origin.y + r * sin(theta))
     }
+    return array
   }
 }
