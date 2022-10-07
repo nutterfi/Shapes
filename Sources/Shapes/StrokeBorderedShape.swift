36f3d1d6bbb9a7c87b94943b03900d6f56ab4bea
Nutter Fi <itsnutterfi@gmail.com>
2022-10-07 14:06:49 -0600

    Added public attributes to StrokeBorderedShape type

--- Sources/Shapes/StrokeBorderedShape.swift
+++ Sources/Shapes/StrokeBorderedShape.swift
@@ -9,28 +9,28 @@ import SwiftUI
 extension Shape {
   
   // returns a Shape that has applied a strokeBorder effect with a given lineWidth and the default StrokeStyle parameters. Applies an optional trim effect.
-  func strokeBordered(_ lineWidth: CGFloat = 1, trim: (CGFloat, CGFloat) = (.zero, 1.0)) -> some Shape {
+  public func strokeBordered(_ lineWidth: CGFloat = 1, trim: (CGFloat, CGFloat) = (.zero, 1.0)) -> some Shape {
     StrokeBorderedShape(shape: self, trim: trim, style: StrokeStyle(lineWidth: lineWidth))
   }
   
   // returns a Shape that has applied a strokeBorder effect with a given StrokeStyle. Applies an optional trim effect.
-  func strokeBordered(_ style: StrokeStyle, trim: (CGFloat, CGFloat) = (.zero, 1.0)) -> some Shape {
+  public func strokeBordered(_ style: StrokeStyle, trim: (CGFloat, CGFloat) = (.zero, 1.0)) -> some Shape {
     StrokeBorderedShape(shape: self, trim: trim, style: style)
   }
 }
 
 /// A wrapper type that provides the capabilities of InsettableShape's strokeBorder methods (excluding ShapeStyle) without converting the Shape to a View
-struct StrokeBorderedShape<Content: Shape>: Shape {
+public struct StrokeBorderedShape<Content: Shape>: Shape {
   /// the Shape to apply the strokeBordered effect (does not need to be Insettable)
-  var shape: Content
+  public var shape: Content
   
   /// trim values to apply to the stroked shape
-  var trim: (CGFloat, CGFloat) = (.zero, 1.0)
+  public var trim: (CGFloat, CGFloat) = (.zero, 1.0)
   
   /// the StrokeStyle to be used when drawing the path
-  var style: StrokeStyle = StrokeStyle()
+  public var style: StrokeStyle = StrokeStyle()
   
-  func path(in rect: CGRect) -> Path {
+  public func path(in rect: CGRect) -> Path {
     let insetRect = rect.insetBy(dx: style.lineWidth / 2, dy: style.lineWidth / 2)
     return shape
       .path(in: insetRect)
