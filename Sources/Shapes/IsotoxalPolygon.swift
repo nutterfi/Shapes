29e9f0d3c5e1d06b2cda7957147c9c7d208536d5
nutterfi <nutterfi@users.noreply.github.com>
2024-02-06 14:22:19 -0800

    IsotoxalPolygon updates
    Enforce even-sided requirement
    Added documentation

--- Sources/Shapes/IsotoxalPolygon.swift
+++ Sources/Shapes/IsotoxalPolygon.swift
@@ -1,29 +1,32 @@
-//
-//  IsotoxalPolygon.swift
-//  
-//
-//  Created by nutterfi on 10/16/21.
-//
-
 import SwiftUI
 
+/// An even-sided equilateral polygon. An isotoxal polygon has symmetry on its edges.
+///
+/// In this implementation, the polygon is constructed with an array of points which are positioned relative to a circle that fits the bounds of the shape. The `innerRadius` is used to compute the position of the even-indexed points. Values less than one position those points toward the shape's center.
 public struct IsotoxalPolygon: Polygon {
+  /// Number of sides. All isotoxal polygons must have even number of sides
   public var sides: Int
+  
+  /// The relative radius of the even-indexed points. Valid for values between 0 and 1.
   public var innerRadius: CGFloat
   
+  /// The inset value of the polygon
   public var inset: CGFloat = .zero
   
-  // sides: should be a positive even number. Undefined behavior if odd numbered
-  // innerRadius: should be between 0...1
-  public init(sides: Int, innerRadius: CGFloat = 1.0) {
-    self.sides = abs(sides)
-    self.innerRadius = innerRadius
+  /// Constructs a new polygon
+  /// - Parameters:
+  ///   - sidePairs: Number of pairs of sides of the polygon
+  ///   - innerRadius: Relative position of the inner points of the polygon, between 0...1
+  public init(sidePairs: Int, innerRadius: CGFloat = 1.0) {
+    self.sides = abs(2 * sidePairs)
+    self.innerRadius = max(0, min(1, innerRadius))
   }
   
+  /// The positions of the polygon corners, ordered by their angular position around the circle. Every even-numbered point is positioned based on the `innerRadius`
   public func vertices(in rect: CGRect) -> [CGPoint] {
     let r = min(rect.size.width, rect.size.height) / 2
     let origin = CGPoint(x: rect.midX, y: rect.midY)
-    return Array(0 ..< sides).map {
+    return (0 ..< sides).map {
       let radius = $0 % 2 == 0 ? r : r * innerRadius
       let theta = 2 * .pi * CGFloat($0) / CGFloat(sides) - .pi / 2 // the origin will now be facing north
       return CGPoint(x: origin.x + radius * cos(theta), y: origin.y + radius * sin(theta))
@@ -37,6 +40,10 @@ public struct IsotoxalPolygon: Polygon {
     }
   }
   
+  @available(iOS 16.0, *)
+  public func sizeThatFits(_ proposal: ProposedViewSize) -> CGSize {
+    Circle().sizeThatFits(proposal)
+  }
   
 }
 
@@ -52,14 +59,32 @@ extension IsotoxalPolygon: Animatable {
 }
 
 struct IsotoxalPolygon_Previews: PreviewProvider {
-  static var previews: some View {
-    ZStack {
-      let innerRadius: CGFloat = 0.5
-      Circle().stroke(Color.gray, lineWidth: 0.5)
-      IsotoxalPolygon(sides: 20, innerRadius: innerRadius)
-        .stroke()
+  
+  struct IsotoxalPolygonDemo: View {
+    @State private var sides: Float = 10
+    @State private var innerRadius: CGFloat = 0.5
+    
+    var body: some View {
+      VStack {
+        Text("Side Pairs: \(Int(sides))")
+        Slider(value: $sides, in: 2...40)
+        
+        Text("InnerRadius: \(String(format: "%.2f", innerRadius))")
+        Slider(value: $innerRadius)
+        
+        IsotoxalPolygon(sidePairs: Int(sides), innerRadius: innerRadius)
+          .stroke()
+          .background {
+            Circle().stroke(Color.gray, lineWidth: 0.5)
+          }
 
+        Spacer()
+      }
+      .padding()
     }
-    .frame(width: 256, height: 512)
+  }
+  
+  static var previews: some View {
+    IsotoxalPolygonDemo()
   }
 }
